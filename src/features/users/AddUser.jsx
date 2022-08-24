d4eea706fead79f3dd8ef91fe72643ccc5e811d3
[Gravitazione] <[core65432@hotmail.com]>
2022-08-24 19:11:35 +0700

    set state set navigation handleuser function styling textfield

--- src/features/users/AddUser.jsx
+++ src/features/users/AddUser.jsx
@@ -1,8 +1,41 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import Button from "../../components/Button"
+import TextField from "../../components/TextField"
 
 const AddUser = () => {
-  return (
-    <div>AddUser</div>
-  )
+
+    const navigate = useNavigate();
+
+    const [values, setValues] = useState({
+        name: '',
+        email: ''
+    });
+
+    const handleAddUser = () => {
+        setValues({ name: '', email: ''});
+        console.log(values);
+        navigate('/');
+    }
+
+    return (
+        <div className={`mt-10 max-w-xl mx-auto`}>
+            <TextField
+                label="Name"
+                value={values.name}
+                onChange={(e) => setValues({ ...values, name: e.target.value })}
+                inputProps={{ type: 'text', placeholder: 'Sasipas Chinjatupornpong' }}
+            />
+            <br />
+            <TextField
+                label="Email"
+                value={values.email}
+                onChange={(e) => setValues({ ...values, email: e.target.value })}
+                inputProps={{ type: 'email', placeholder: 'test@gmail.com' }}
+            />
+            <Button onClick={handleAddUser}>Submit</Button>
+        </div>
+    )
 }
 
 export default AddUser
