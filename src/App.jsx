1bdee7ef7652315bf475ad2fc447a485b8b6901e
[Gravitazione] <[core65432@hotmail.com]>
2022-08-24 17:30:58 +0700

    test react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import UserList from './features/users/UserList'
+import { Route, Routes } from 'react-router-dom'
+import AddUser from './features/users/AddUser'
 
 function App() {
 
   return (
     <div className={`container mx-auto px-2 max-w-5xl pt-10 md:pt-32`}>
       <h1 className={`text-center font-bold text-2xl text-gray-700`}>CRUD Application</h1>
-      <UserList />
+      <Routes>
+        <Route path='/' element={<UserList />} />
+        <Route path='/add-user' element={<AddUser />} />
+      </Routes>
     </div>
   )
 }
