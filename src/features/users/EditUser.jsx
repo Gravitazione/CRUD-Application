e1f8f0f7e23dc9cb056ecc69caeb0a307a119534
[Gravitazione] <[core65432@hotmail.com]>
2022-08-24 19:47:38 +0700

    set edit user

--- src/features/users/EditUser.jsx
+++ src/features/users/EditUser.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom'
+import Button from "../../components/Button";
+import TextField from "../../components/TextField";
 
 const EditUser = () => {
-  return (
-    <div>EditUser</div>
-  )
+  
+    const navigate = useNavigate();
+
+    const [values, setValues] = useState({
+        name: '',
+        email: ''
+    });
+
+    const handleEditUser = () => {
+        setValues({ name: '', email: ''});
+        console.log(values);
+        navigate('/');
+    }
+
+    return (
+        <div className={`mt-10 max-w-xl mx-auto`}>
+            <TextField
+                label="Name"
+                value={values.name}
+                onChange={(e) => setValues({ ...values, name: e.target.value })}
+                inputProps={{ type: 'text', placeholder: 'Sasipas Chinjatupornpong' }}
+            />
+            <br />
+            <TextField
+                label="Email"
+                value={values.email}
+                onChange={(e) => setValues({ ...values, email: e.target.value })}
+                inputProps={{ type: 'email', placeholder: 'test@gmail.com' }}
+            />
+            <Button onClick={handleEditUser}>Edit</Button>
+        </div>
+    )
 }
 
 export default EditUser
